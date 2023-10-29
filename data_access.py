ed33bea91e72f857f337e854c5071f7dc194a0f2
modengann <99417989+modengann@users.noreply.github.com>
2023-10-29 13:03:58 -0400

    init

---
+++ data_access.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+def get_age(df):
+    pass
+
+def get_name_and_occupation(df):
+    pass
+
+def get_third_name(df):
+    pass
+
+def age_salary_of_first_two(df):
+    pass
+
+def carol_deets(df):
+    pass
+
+def occupation_bob_eve(df):
+    pass
+
+
+    
+
+def main():
+    df = pd.read_csv("data.csv", index_col = 0)
+    print(df)
+    
+    
+
+if __name__ == "__main__":
+    main()
